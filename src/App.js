db82c55253c35fc7789a447c9d97625a7f64ab1a
JonathanWVelasco <95450703+JonathanWVelasco@users.noreply.github.com>
2021-12-04 15:12:49 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <h1>Team Agone</h1>
       <div class="center">
 	  <h1>"Wow, I am empty :)!"</h1>
+	  <a href="https://testbranch.d5mz5bs8uvjp6.amplifyapp.com/"> Redirect </a>
       </div>  
     </div>
   );
