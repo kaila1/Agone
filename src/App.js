eea55d27d93a09a26c03d441c3e609117614416f
JonathanWVelasco <95450703+JonathanWVelasco@users.noreply.github.com>
2021-12-04 15:17:06 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <h1>Team Agone</h1>
       <div class="center">
 	  <h1>"Wow, I am empty :)!"</h1>
-	  <a href="https://testbranch.d5mz5bs8uvjp6.amplifyapp.com/"> Redirect </a>
+	  <a href="https://master.d5mz5bs8uvjp6.amplifyapp.com/"> Redirect to Main Page</a>
       </div>  
     </div>
   );
