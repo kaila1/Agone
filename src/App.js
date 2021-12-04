8602cdc2895eaef2b300ca420828bc30825a6712
JonathanWVelasco <jaw645@nau.edu>
2021-12-04 13:16:09 -0700

    Tabs edit v3 test

--- src/App.js
+++ src/App.js
@@ -8,142 +8,100 @@ function App() {
       <h1>Team Agone</h1>
       <Tabs>
         <div label="Student Data">
-          <table>
-            <tr>
-              <th>
-                Athlete
-              </th>
-              <th>
-                Phone Number
-              </th>
-              <th>
-                Distance Ran(Miles)
-              </th>
-              <th>
-                Wellness Report
-              </th>
-            </tr>
-            <tr>
-              <th>
-                Jonathan W.
-              </th>
-              <th>
-                555-4525
-              </th>
-              <th>
-                10.5
-              </th>
-              <th>
-                Not feeling well today
-              </th>
-            </tr>
-            <tr>
-              <th>
-                Kaila I.
-              </th>
-              <th>
-                555-9357
-              </th>
-              <th>
-                15
-              </th>
-              <th>
-                No issues today
-              </th>
-            </tr>
-          </table>
+        <table>
+    	    <tr>
+        	  <th>Athlete</th>
+        	  <td>Rylee</td>
+        	  <td>Sam</td>
+        	  <td>Jonathan</td>
+        	  <td>Kaila</td>
+    	    </tr>
+    	    <tr>
+        	  <th>Phone Number</th>
+        	  <td>555-9548</td>
+        	  <td>555-3490</td>
+        	  <td>555-8036</td>
+        	  <td>555-4958</td>
+    	    </tr>
+    	    <tr>
+      	    <th>Distance Ran(Miles)</th>
+      	    <td>19.3</td>
+            <td>8.7</td>
+        	  <td>10.5</td>
+      	    <td>15</td>
+  	      </tr>
+  	      <tr>
+      	    <th>Wellness Report</th>
+        	  <td>School has been stressing me out.</td>
+        	  <td>Overall Good.</td>
+            <td>Great, better than last week for sure.</td>
+        	  <td>No issues today.</td>
+    	    </tr>
+    	  </table>
         </div>
         <div label="WORKOUT PLAN">
         <table>
-            <tr>
-              <th>
-                Athlete
-              </th>
-              <th>
-                Current Weekly Workout
-              </th>
-              <th>
-                Workout Progress percentage
-              </th>
-              <th>
-                Athelte comments on current workout
-              </th>
-            </tr>
-            <tr>
-              <th>
-                Jonathan W.
-              </th>
-              <th>
-                10 mile run per week
-              </th>
-              <th>
-                105%
-              </th>
-              <th>
-                Satisfied with current workout.
-              </th>
-            </tr>
-            <tr>
-              <th>
-                Kaila I.
-              </th>
-              <th>
-                15 mile run per week
-              </th>
-              <th>
-                100%
-              </th>
-              <th>
-                Increase current workout please!
-              </th>
-            </tr>
-          </table>
+    	    <tr>
+        	  <th>Athlete</th>
+        	  <td>Rylee</td>
+        	  <td>Sam</td>
+        	  <td>Jonathan</td>
+        	  <td>Kaila</td>
+    	    </tr>
+    	    <tr>
+        	  <th>Current Weekly Workout</th>
+        	  <td>20 Miles Running</td>
+        	  <td>10 Miles Running</td>
+        	  <td>10 Miles Jogging</td>
+        	  <td>15 Miles Running</td>
+    	    </tr>
+    	    <tr>
+        	  <th>Workout Progress Percentage</th>
+        	  <td>96.5%</td>
+        	  <td>87%</td>
+        	  <td>105%</td>
+        	  <td>100%</td>
+    	    </tr>
+    	    <tr>
+        	  <th>Comments on Workout</th>
+          	  <td>Didnt meet weekly goal, but workout is fine.</td>
+          	  <td>Reduce workout please!</td>
+              <td>Workout is good as is.</td>
+        	  <td>Increase workout distance.</td>
+    	    </tr>
+    	  </table>
         </div>
         <div label="INJURY DASHBOARD">
         <table>
-            <tr>
-              <th>
-                Athlete
-              </th>
-              <th>
-                Injury History
-              </th>
-              <th>
-                Type of Injuries
-              </th>
-              <th>
-                Date of Injury
-              </th>
-            </tr>
-            <tr>
-              <th>
-                Jonathan W.
-              </th>
-              <th>
-                Yes
-              </th>
-              <th>
-                torn ACL, cracked rib
-              </th>
-              <th>
-                1/15/2015, 3/5/2020
-              </th>
-            </tr>
-            <tr>
-              <th>
-                Kaila I.
-              </th>
-              <th>
-                Yes
-              </th>
-              <th>
-                Broken leg
-              </th>
-              <th>
-                5/27/2019
-              </th>
-            </tr>
-          </table>
+    	    <tr>
+        	  <th>Athlete</th>
+        	  <td>Rylee</td>
+        	  <td>Sam</td>
+        	  <td>Jonathan</td>
+        	  <td>Kaila</td>
+    	    </tr>
+    	    <tr>
+        	  <th>Injury History</th>
+        	  <td>Yes</td>
+        	  <td>No</td>
+        	  <td>Yes</td>
+        	  <td>Yes</td>
+    	    </tr>
+    	    <tr>
+        	  <th>Type of Injuries</th>
+        	  <td>Sprained Ankle</td>
+        	  <td>-</td>
+        	  <td>Cracked Rib, Torn ACL</td>
+        	  <td>Dislocated Shoulder</td>
+    	    </tr>
+    	    <tr>
+        	  <th>Date of Injury</th>
+          	  <td>9/30/2015</td>
+          	  <td>-</td>
+              <td>11/21/2019, 3/13/2020</td>
+        	  <td>6/3/2018</td>
+    	    </tr>
+    	  </table>
         </div>
       </Tabs>
     </div>
